4cab26d460baf5d50e0cd27f5ae9d3ae8e5b2201
Nullify <070Nooaf@gmail.com>
2019-10-18 07:46:11 +0300

    Create Random-img-String.js

---
+++ Random-img-String.js
@@ -0,0 +1,17 @@
+var NameArrayRandom = [
+    " Any String Or src For Photo ",
+    " Any String Or src For Photo ",
+    " Any String Or src For Photo ",
+    " Any String Or src For Photo "
+    ],
+    RandomNum = Math.floor(Math.random() * Ayat.length);
+
+    function Random(){
+          $(".Ayaa-text p").text(NameArrayRandom[RandomNum]);
+
+      };
+
+      Random();
+    
+
+
